0b435aae91e9b02cd5bffd47a84b675185e18465
Taras Chuchvara <t.chuchvara@tenantcloud.com>
2021-07-13 01:12:07 +0300

    fix: Added registrations in service provider

--- src/TenantCloud/Providers/MixinsServiceProvider.php
+++ src/TenantCloud/Providers/MixinsServiceProvider.php
@@ -2,8 +2,24 @@
 
 namespace TenantCloud\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Auth\RequestGuard;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Query\Grammars\MySqlGrammar;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
+use TenantCloud\Mixins\ArrMixin;
+use TenantCloud\Mixins\CarbonMixin;
+use TenantCloud\Mixins\CollectionMixin;
+use TenantCloud\Mixins\EloquentBuilderMixin;
+use TenantCloud\Mixins\MySqlGrammarMixin;
+use TenantCloud\Mixins\QueryBuilderMixin;
+use TenantCloud\Mixins\RequestGuardMixin;
+use TenantCloud\Mixins\RequestMixin;
 use TenantCloud\Mixins\StrMixin;
 
 class MixinsServiceProvider extends ServiceProvider
@@ -16,5 +32,21 @@ class MixinsServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		Str::mixin(new StrMixin());
+		Arr::mixin(new ArrMixin());
+		Carbon::mixin(new CarbonMixin());
+		Collection::mixin(new CollectionMixin());
+		MySqlGrammar::mixin(new MySqlGrammarMixin());
+		QueryBuilder::mixin(new QueryBuilderMixin());
+		Request::mixin(new RequestMixin());
+		RequestGuard::mixin(new RequestGuardMixin());
+
+		// Because Eloquent Builder doesn't work with mixins.
+		EloquentBuilder::macro('withHas', (new EloquentBuilderMixin())->withHas());
+		EloquentBuilder::macro('withAggregate', (new EloquentBuilderMixin())->withAggregate());
+		EloquentBuilder::macro('withSum', (new EloquentBuilderMixin())->withSum());
+		EloquentBuilder::macro('selectCaseIn', (new EloquentBuilderMixin())->selectCaseIn());
+		EloquentBuilder::macro('customWhereNested', (new EloquentBuilderMixin())->customWhereNested());
+		EloquentBuilder::macro('conditionalUpdate', (new EloquentBuilderMixin())->conditionalUpdate());
+		EloquentBuilder::macro('toRawSql', (new EloquentBuilderMixin())->toRawSql());
 	}
 }
