120ae485b5dd105835e3d887d5a923bcb1cee808
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2022-02-16 18:40:12 +0200

    fix: Always serialize the same data under the same key

--- src/TenantCloud/Mixins/Services/CacheSerializer.php
+++ src/TenantCloud/Mixins/Services/CacheSerializer.php
@@ -4,13 +4,20 @@ namespace TenantCloud\Mixins\Services;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
+use Tests\Services\CacheSerializerTest;
 
+/**
+ * @see CacheSerializerTest
+ */
 class CacheSerializer
 {
 	public static function serialize($object, callable $serializeCallback = null): array
 	{
 		$serializeCallback ??= fn ($object) => serialize($object);
-		Cache::put($key = uniqid('mixins:serialized:', false), $serializeCallback($object), Carbon::now()->addWeek());
+		$serialized = $serializeCallback($object);
+
+		// todo: migrate to murmurhash3 once this requires php >= 8.1
+		Cache::put($key = hash('sha256', $serialized), $serialized, Carbon::now()->addWeek());
 
 		return ['key' => $key];
 	}
---
+++ tests/Services/CacheSerializerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Services;
+
+use Generator;
+use stdClass;
+use TenantCloud\Mixins\Services\CacheSerializer;
+use Tests\TestCase;
+
+/**
+ * @see CacheSerializer
+ */
+class CacheSerializerTest extends TestCase
+{
+	public static function serializesSameDataTwiceUnderTheSameKeyProvider(): Generator
+	{
+		yield [
+			'64f225676093805c64e7fe5b289434d2a6159094af89e83474328eaf1cf67fa8',
+			value(function () {
+				$data = new stdClass();
+				$data->name = 'test';
+
+				return $data;
+			}),
+		];
+
+		yield [
+			'ea1c3ec9193a908b1f0921c772cff4428b90827f223f9106c3bb6368ba5c352b',
+			value(function () {
+				$data = new stdClass();
+				$data->name = 'test2';
+
+				return $data;
+			}),
+		];
+	}
+
+	/**
+	 * @dataProvider serializesSameDataTwiceUnderTheSameKeyProvider
+	 */
+	public function testSerializesData(string $expectedKey, object $data): void
+	{
+		self::assertSame($expectedKey, CacheSerializer::serialize($data)['key']);
+	}
+}
