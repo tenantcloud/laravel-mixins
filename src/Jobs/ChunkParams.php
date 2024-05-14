822ff4d30d22a10bfa8d0a9d2cd4ad76b9fb11cb
oprypkhantc <54406427+oprypkhantc@users.noreply.github.com>
2024-05-14 20:12:58 +0000

    chore: Sync changes from the template

--- src/Jobs/ChunkParams.php
+++ src/Jobs/ChunkParams.php
@@ -10,35 +10,16 @@ use TenantCloud\Mixins\Queue\Handlers\Serializable\Handler;
  */
 class ChunkParams
 {
-	/** @var Handler<TModel> */
-	public Handler $handler;
-
-	public string $key;
-
-	public string $keyAttributeName;
-
-	public int $chunkSize;
-
-	public int $pieceSize;
-
-	public ?string $queue;
-
 	/**
 	 * @param Handler<TModel> $handler
 	 */
 	public function __construct(
-		Handler $handler,
-		string $key,
-		string $keyAttributeName,
-		int $chunkSize,
-		int $pieceSize,
-		string $queue = null
-	) {
-		$this->handler = $handler;
-		$this->key = $key;
-		$this->keyAttributeName = $keyAttributeName;
-		$this->chunkSize = $chunkSize;
-		$this->pieceSize = $pieceSize;
-		$this->queue = $queue;
-	}
+		/** @var Handler<TModel> */
+		public Handler $handler,
+		public string $key,
+		public string $keyAttributeName,
+		public int $chunkSize,
+		public int $pieceSize,
+		public ?string $queue = null
+	) {}
 }
--- tests/EloquentBuilderMixin/Stubs/HandlerStub.php
+++ tests/EloquentBuilderMixin/Stubs/HandlerStub.php
@@ -14,7 +14,7 @@ class HandlerStub implements QueuedChunkHandler
 {
 	protected ?SerializableClosure $callback;
 
-	public function __construct(callable $callback = null)
+	public function __construct(?callable $callback = null)
 	{
 		$this->callback = $callback ? new SerializableClosure($callback) : null;
 	}
